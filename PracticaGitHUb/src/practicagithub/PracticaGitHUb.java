7a403548f08094bec86e4785e832caae74f0fa93
mata889 <dam_900@unitec.edu>
2017-07-27 07:39:31 -0600

    agregue algo

---
+++ PracticaGitHUb/src/practicagithub/PracticaGitHUb.java
@@ -0,0 +1,12 @@
+package practicagithub;
+
+public class PracticaGitHUb {
+
+    public static void main(String[] args) {
+        System.out.println("Cheq");
+        int nu = 0;
+        do {
+        } while (nu != 8);
+    }
+
+}
